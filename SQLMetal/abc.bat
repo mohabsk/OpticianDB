d5a0d16f1f2c27ecaa72b3a60bc166934fcd08e0
gsp8181 <gsp8181@users.noreply.github.com>
2011-04-04 17:17:00 +0000

    Final submitted version
    git-svn-id: https://opticiandb.googlecode.com/svn/trunk@46 930474a0-0457-b4a5-f14f-5682c71e1d18

--- SQLMetal/abc.bat
+++ SQLMetal/abc.bat
@@ -1,4 +1,4 @@
 @echo off
-C1\SQLCreate.exe ../blankdb.sql
+C1\SQLCreate.exe ../Resources/blankdb.sql
 C2\DBMetal.exe /namespace:OpticianDB /provider:SQLite "/conn:Data Source=blankdb.db3" /database=OpticianDB /code=DBAdaptor.cs
 del blankdb.db3
